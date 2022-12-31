82de8afb51f142f57095be1192a6fb0b6b2ea118
Robolta <ameliakgreenwood@gmail.com>
2022-12-30 23:51:53 -0500

    Switched used to HashSet for significant speedup.

--- src/optimizations/pair_vec.rs
+++ src/optimizations/pair_vec.rs
@@ -1,42 +1,45 @@
 use crate::constants::*;
 use crate::layers::*;
+use std::collections::HashSet;
 
 /*
 Generates a vector of valid layers to follow each layer in unique
 key: layer ([i16; STATES as usize]), value: unique indexes, index in current table (Vec<[usize; 2]>)*/
-pub fn generate_pairs (unique: &Vec<[i16; STATES as usize]>, mcount: usize) -> Vec<Vec<[usize; 2]>> { // Consider some form of handling for layers which have no valid layer after
+pub fn generate_pairs (unique: &Vec<[i16; STATES as usize]>) -> Vec<Vec<[usize; 2]>> { // Consider some form of handling for layers which have no valid layer after
     let mut target_groups: Vec<i16> = Vec::new();
     for i in TARGET {
         if !target_groups.contains(&(i as i16)) {
             target_groups.push(i as i16);
         }
     }
+    let target_groups = target_groups.len();
 
-    let mut used: Vec<[i16; STATES as usize]> = Vec::new();
+    //let mut used: Vec<[i16; STATES as usize]> = Vec::new();
+    let mut used: HashSet<[i16; STATES as usize]> = HashSet::new();
     let mut identity: [i16; STATES as usize] = [0; STATES as usize];
     for i in 0..STATES {
         identity[i as usize] = i;
     }
-    used.push(identity); // Don't include identity outputs
+    used.insert(identity);
+    //used.push(identity); // Don't include identity outputs
 
     for i in unique { // Don't include cases which give the same output as single layers
-        used.push(*i);
+        //used.push(*i);
+        used.insert(*i);
     }
 
     let mut pairs: Vec<Vec<[usize; 2]>> = Vec::new();
-    for i in 0..mcount {
+    for first in unique.iter() {
 
-        let first: [i16; STATES as usize] = unique[i];
-        //let mut cpairs: Vec<[i16; STATES as usize]> = Vec::new();
         let mut cpairs: Vec<[usize; 2]> = Vec::new();
-        for j in 0..mcount {
+        for (index, second) in unique.iter().enumerate() {
             
-            let second: [i16; STATES as usize] = unique[j];
-            let pass: [i16; STATES as usize] = through(second, first);
+            let pass: [i16; STATES as usize] = through(*second, *first);
 
-            if groups(pass) >= target_groups.len() && !used.contains(&pass) {
-                cpairs.push([j, cpairs.len()]); // [Unique Index, Pairs Element Index]
-                used.push(pass);
+            if groups(pass) >= target_groups && !used.contains(&pass) {
+                cpairs.push([index, cpairs.len()]); // [Unique Index, Pairs Element Index]
+                //used.push(pass);
+                used.insert(pass);
             }
         }
 
